64aa39e9ad3a5030bbecd644497c476bfaff7daa
luming60 <2320536239@qq.com>
2018-04-06 14:12:44 +0800

    syso too

--- src/gdfs/fsff.java
+++ src/gdfs/fsff.java
@@ -5,6 +5,7 @@ public class fsff {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("I Love You!");
+		System.out.println("I Love You too£¡");
 
 	}
 }
