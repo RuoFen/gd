59e3e3af372d2e8abd78c26c826ebfc0156beea2
luming60 <2320536239@qq.com>
2018-04-06 14:49:26 +0800

    删除 too

--- src/gdfs/fsff.java
+++ src/gdfs/fsff.java
@@ -5,7 +5,6 @@ public class fsff {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("I Love You!");
-		System.out.println("I Love You tooŁĄ");
 		System.out.println("I Love You 4");
        	System.out.println("I Love You 5");
        	System.out.println("I Love You 六六六");
